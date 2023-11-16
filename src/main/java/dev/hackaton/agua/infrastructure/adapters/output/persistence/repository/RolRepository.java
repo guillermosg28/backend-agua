18165589f54d3f48744cee13b650c5b2082c6df4
ingrid <ingrid.alvarez.alonso@outlook.com>
2023-11-16 13:27:41 +0100

    RolRepository Añadido

---
+++ src/main/java/dev/hackaton/agua/infrastructure/adapters/output/persistence/repository/RolRepository.java
@@ -0,0 +1,15 @@
+package dev.hackaton.agua.infrastructure.adapters.output.persistence.repository;
+
+import dev.hackaton.agua.infrastructure.adapters.output.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+    /**
+     * The interface User repository.
+     */
+
+    @Repository
+    public interface RolRepository extends JpaRepository<UserEntity, Long> {
+
+        UserEntity findByEmailPAndPassword(String email, String password);
+}
