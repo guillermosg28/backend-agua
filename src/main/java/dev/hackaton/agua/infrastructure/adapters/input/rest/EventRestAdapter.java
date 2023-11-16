a7a20c2f1f96c6689a02f03d1c36c3e560f2f514
guillermosg28 <guillermo_sg_12@hotmail.com>
2023-11-16 23:28:34 +0100

    add

--- src/main/java/dev/hackaton/agua/infrastructure/adapters/input/rest/EventRestAdapter.java
+++ src/main/java/dev/hackaton/agua/infrastructure/adapters/input/rest/EventRestAdapter.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1/")
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class EventRestAdapter implements EventApi{
 
     private final EventUseCase eventUseCase;
